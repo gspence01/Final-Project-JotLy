43ca5061e467a575270a8bfea39f33caf361c321
Gabrielle <gabrielle01.spence@gmail.com>
2023-05-24 21:00:31 -0700

    More styling of home page

--- frontend/src/components/SplashPage.js
+++ frontend/src/components/SplashPage.js
@@ -3,9 +3,10 @@ import NaviBar from "./NaviBar"
 export default function SplashPage(){
     return(
         <div>
-            <NaviBar className="navbar" style={{zIndex:'1'}}/>
-            <div className='background-pic'>
-                <h1 style={{fontSize:'200px', justifySelf:'center', fontFamily: 'Amatic SC, cursive', color: 'white'}}>JotLy</h1>
+            <NaviBar className="navbar" style={{position:'absolute', zIndex:'1'}}/>
+            <div className='background-pic' style={{zIndex:'-1', position:'absolute', top:'0rem'}}>
+                <h1 style={{fontSize:'200px', margin:'auto', fontFamily: 'Amatic SC, cursive', color: 'white'}}>JotLy</h1>
+                <h5 style={{fontSize:'12px',color:'white'}}>Photo by <a style={{color:'white'}}href='https://unsplash.com/@sixteenmilesout'>Sixteen Miles Out</a> on Unsplash</h5>
             </div>
         </div>
     )
