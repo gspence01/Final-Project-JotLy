6bcb25ba0b99fc1e620c445cb84e1585501d51f6
Gabrielle <gabrielle01.spence@gmail.com>
2023-05-25 20:02:20 -0700

    made modal

--- frontend/src/components/NaviBar.js
+++ frontend/src/components/NaviBar.js
@@ -1,20 +1,46 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
-import Nav from 'react-bootstrap/Nav'
+import Nav from 'react-bootstrap/Nav';
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
 
 export default function NaviBar(){
+    //Modal setup thanks to https://react-bootstrap.netlify.app/docs/components/modal/
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleOpen = () => setShow(true);
+
     return (
         <>
             <Navbar>
                 <Container>
                     <Navbar.Brand href="#home">JotLy</Navbar.Brand>
                     <Nav>
-                        <Button variant='outline-info'>Sign Up</Button>
-                        <Button variant='success'>Log In</Button>
+                        <Button variant='outline-info' onClick={handleOpen}>Sign Up</Button>
+                        <Button variant='success' onClick={handleOpen}>Log In</Button>
                     </Nav>
                 </Container>
             </Navbar>
+            
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Welcome to JotLog!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form>
+                        <label htmlFor='username'>Username:</label><br />
+                        <input type='text' name='username' /><br/>
+                        <label htmlFor='pass'>Password:</label><br />
+                        <input type='password' name='pass'/>
+                        
+                    </form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <p>hi</p>
+                </Modal.Footer>
+            </Modal>
         </>
     )
 }
