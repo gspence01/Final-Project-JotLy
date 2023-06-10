c81f608f98447e2774843d7a0cb52475e1f141e4
Gabrielle <gabrielle01.spence@gmail.com>
2023-06-10 00:03:46 -0700

    added delete function

--- frontend/src/components/PostCard.js
+++ frontend/src/components/PostCard.js
@@ -1,9 +1,30 @@
+import { useContext } from "react"
+import { CurrentUser } from "../context/CurrentUser"
+
 export default function PostCard(props) {
+    const user = useContext(CurrentUser).currentUser
+
+    const deletePost = async (id) => {
+        await fetch(`http://localhost:8801/entries/${id}`, {
+            method: 'DELETE',
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+
+    const deleteButton = () => {
+        return <button onClick={deletePost.bind(this, props.item.entry_id)}>delete</button>
+    }
+
     return (
         <div id="post-card" style={{padding:'10px', margin:'20px'}}>
             <h3><strong>{props.item.title}</strong></h3>
             <p>{props.item.username} is feeling {props.item.feeling}</p>
             <p>{props.item.content}</p>
+            <div>
+                {user.user_id === props.item.user_id ? deleteButton() : null}
+            </div>
         </div>
     )
 }
